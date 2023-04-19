77132f7abf55d74c7af18425626118ee5d075a9a
Manuel Avila <magolitonft2311@gmail.com>
2023-04-19 10:23:19 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,36 @@
+function divide(a, b) {
+    return a / b;
+}
+const result = divide(2000, 1000);
+console.log(result);
+
+
+let square = x => x * x;
+
+
+let add = (a, b) => a + b;
+
+//function divide(a, b) {
+//    return a / b;
+//}
+//const result = divide (2000, 1000);
+//console.log(result);
+
+
+//function square(x) {
+//    return x * x;
+//}
+
+//const NaN = square(2);
+//console.log(NaN);
+
+
+
+//function add(a, b) {
+//   return a + b;
+//};
+
+//const answer = add(5, 4);
+//console.log(answer);
+
+
